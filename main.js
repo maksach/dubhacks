199fb6231aa3dcecc2c8f399eccd06d04b516242
Mikur <michael.trinh1996@gmail.com>
2015-10-18 18:17:49 -0700

    Removed uncessary scripts from html file JS: works if use when for the document ready function, but doesn't work if make its own function BUG

--- main.js
+++ main.js
@@ -4,115 +4,128 @@
 //  10/18/15
 //  Dubhacks project with Clarifai and Yelp APIs
 
-"use strict";
+var wordTags = [];
+var allTags;
 
-(function()
+$(document).ready(function()
+	{
+		/*$.when(getTags('http://www.shakengolynnwood.com/wp-content/uploads/2015/01/Chicken-Burger2.jpg'))
+		.then(function(r)
+		{
+			
+		});*/
+	}
+);
+
+
+
+/*$(document).ready(function()
+		{
+     	clarifai = new Clarifai(
+        	{
+            	'clientId': 'T6rt9-xMGrovXB_LTpsBx0dOuWgcOO-SRIi34ezI',
+            	'clientSecret': 'fWeVSQp-297v69lUT66XN7Vt7AOIBg4QNneljkvU' 
+        	}
+      	);
+		}  
+);
+
+function positive(imgurl) 
 {
-	var clarifai;
-	var baseUrl = 'https://api-alpha.clarifai.com/v1/';
-
-	window.onload = function()
-	{
-
-	};
-
-	$(document).ready(function()
-  		{
-	     	clarifai = new Clarifai(
-	        	{
-	            	'clientId': 'T6rt9-xMGrovXB_LTpsBx0dOuWgcOO-SRIi34ezI',
-	            	'clientSecret': 'fWeVSQp-297v69lUT66XN7Vt7AOIBg4QNneljkvU' 
-	        	}
-	      	);
-  		}  
+	clarifai.positive(imgurl, 'ferrari', callback).then(
+   		promiseResolved,
+    	promiseRejected
 	);
-
-	function positive(imgurl) 
-	{
-		clarifai.positive(imgurl, 'ferrari', callback).then(
-	   		promiseResolved,
-	    	promiseRejected
-		);
-	}
-
-	function negative(imgurl) 
-	{
-		clarifai.negative(imgurl, 'ferrari', callback).then(
-	    	promiseResolved,
-	    	promiseRejected
-	  	);
-	}
-
-	function train() {
-		clarifai.train('ferrari', callback).then(
-			promiseResolved,
-			promiseRejected
-		);
-	}
-
-	function predict(imgurl) {
-		clarifai.predict(imgurl, 'ferrari', callback).then(function(obj) {
-			if (obj.score < 0.6) 
-			{
-				swal({
-					title: 'WHAT!',
-					text: 'Get that car out of my face.',
-					imageUrl: obj.url
-				});
-			} else {
-				swal({
-					title: 'Sweet!',
-					text: 'This car is worthy.',
-					imageUrl: obj.url
-				});
-			}
-		}, promiseRejected);
-	}
-
-	function promiseResolved(obj)
-	{
-		console.log('Promise resolved', obj);
-	}
-
-	function promiseRejected(obj)
-	{
-		console.log('Promise rejected', obj);
-	}
-
-	function callback(obj)
-	{
-		console.log('callback', obj);
-	}
-
-	function urlSubmit()
-	{
-		getTags($('#url').val());
-	}
-
-    function getTags(url)
-    {
-    	var data = {
-    		"urls": [url]
-    	}
-    	$.ajax(
-    		{	
-	            'type': 'POST',
-	            'contentType': 'application/json; charset=utf-8',
-	            'processData': false,
-	            'data': JSON.stringify(data),
-	            'success': function(data)
-	            {
-	            	console.log(data);
-	            },
-	            'url': this.baseUrl + '/tag/?url=' + url,
-	            'headers': {
-	                'Authorization': 'Bearer ' + 'tYdYLZL5Wv2pnleyjR6KAZlyZkQagE'
-	            }
-    		}
-    	).then(
-    		
-    	);
-    }
-
 }
-)();
+
+function negative(imgurl) 
+{
+	clarifai.negative(imgurl, 'ferrari', callback).then(
+    	promiseResolved,
+    	promiseRejected
+  	);
+}
+
+function train() {
+	clarifai.train('ferrari', callback).then(
+		promiseResolved,
+		promiseRejected
+	);
+}
+
+function predict(imgurl) {
+	clarifai.predict(imgurl, 'ferrari', callback).then(function(obj) {
+		if (obj.score < 0.6) 
+		{
+			swal({
+				title: 'WHAT!',
+				text: 'Get that car out of my face.',
+				imageUrl: obj.url
+			});
+		} else {
+			swal({
+				title: 'Sweet!',
+				text: 'This car is worthy.',
+				imageUrl: obj.url
+			});
+		}
+	}, promiseRejected);
+}
+
+function promiseResolved(obj)
+{
+	console.log('Promise resolved', obj);
+}
+
+function promiseRejected(obj)
+{
+	console.log('Promise rejected', obj);
+}
+
+function callback(obj)
+{
+	console.log('callback', obj);
+}*/
+
+function urlSubmit()
+{
+	getTags($("#url").val());
+
+	for (var i = 0; i < 5; i++)
+	{
+		allTags += wordTags[i] + '+'; 
+	}
+	allTags += wordTags[5];
+	window.location = 'http://www.yelp.com/search?find_desc=' + allTags + '&find_loc';
+
+	console.log("urlsubmit");
+}
+
+function getTags(url)
+{
+	console.log("gettags");
+	var data = {
+		'url': url
+	}
+
+	$.ajax(
+		{	
+			'type': 'POST',
+            'data': data,
+            'url': 'https://api.clarifai.com/v1/tag/',
+            'headers': {
+                'Authorization': 'Bearer ' + 'tYdYLZL5Wv2pnleyjR6KAZlyZkQagE'
+            }
+		}
+	).then(
+		function(response)
+		{
+			wordTags = response.results[0].result.tag.classes;
+			for (var i = 0; i < 5; i++)
+			{
+				allTags += wordTags[i] + '+';
+			}
+			allTags += wordTags[5];
+		}
+	);
+}
